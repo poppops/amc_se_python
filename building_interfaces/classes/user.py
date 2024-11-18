fc4af34f3f018e31f2bf724a64f8b3aae0b56bfe
Suhmayah Banda <s.banda911@googlemail.com>
2024-11-18 12:45:27 +0000

    Implemented user.retrieve()
    Interface now uses the class methods to save and load user information

--- building_interfaces/classes/user.py
+++ building_interfaces/classes/user.py
@@ -1,40 +1,53 @@
+import os
+
+
 class User:
-    '''A user class to represent the users of our application'''
-    
+    """A user class to represent the users of our application"""
+
     dob = "01/01/1970"
     password = "password"
-    
+
     def __init__(self, firstName, lastName):
         self.firstName = firstName
         self.lastName = lastName
-    
+
     def create(self):
         # Save the user information to a file
         # Create a file to save the user information
         try:
             userFile = open(self.getUserFileName(), "w")
             # Write the user information to the file
-            # userFile.write(f"firstName:{self.firstName}\n")
-            # userFile.write(f"lastName:{self.lastName}\n")
-            # userFile.write(f"dob:{self.dob}\n")
-            # userFile.write(f"password:{self.password}\n")
-            
             # Implement writing to file using a loop with the dir() function
             for attr in dir(self):
-                if attr[0] != "_" and attr not in ('create', 'retrieve', 'getUserFileName'):
+                if attr[0] != "_" and attr not in (
+                    "create",
+                    "retrieve",
+                    "getUserFileName",
+                ):
                     userFile.write(f"{attr}:{getattr(self, attr)}\n")
-                    
+
             # Close the file
             userFile.close()
             # Provide feedback
             return True
         except:
             return False
-            
-        
-    def retrieve(self):
+
+    def retrieve(self, password):
         # Loads the user information from a file
-        print("The user information has been loaded")
-        
+        if not os.path.exists(self.getUserFileName()):
+            return False
+
+        with open(self.getUserFileName(), "r") as file:
+            for line in file:
+                key, value = line.split(":")
+                setattr(self, key, value.strip())
+
+        if password.strip() == self.password:
+            return True
+        else:
+            print("Passwords didn't match", f"{password}:{self.password}")
+            return False
+
     def getUserFileName(self):
         return f"{self.firstName}_{self.lastName}.txt"
--- building_interfaces/user_registration_app2.py
+++ building_interfaces/user_registration_app2.py
@@ -3,47 +3,38 @@ from tkinter import messagebox
 import os
 from classes.user import User
 
+
 def saveUserInfo():
     # Create a new user object
     newUser = User(fnEntry.get(), lnEntry.get())
-
     # get values from inputs
     newUser.dob = DOBEntry.get()
     newUser.password = pwEntry.get()
-    
+
     # write information to a file
     if newUser.create():
         messagebox.showinfo("Success!", "Information has been saved successfully")
     else:
         messagebox.showerror("Something went wrong!", "There was an error while saving")
 
+
 def loadUserInfo():
     fName = fnEntry.get()
     lName = lnEntry.get()
     pword = pwEntry.get()
-    
-    if not os.path.exists(f"{fName}_{lName}.txt"):
+
+    user = User(fName, lName)
+    if user.retrieve(pword):
+        messageTitle = f"{user.firstName} {user.lastName} Account"
+        messageContent = (
+            f"Account found for {user.firstName} {user.lastName}: {user.dob}"
+        )
+        messagebox.showinfo(messageTitle, messageContent)
+    else:
         errorTitle = "No User Found"
         errorMessage = f"Could not find information for {fName} {lName}"
         messagebox.showerror(errorTitle, errorMessage)
-    else:
-        # Create a dictionary to hold user information
-        userInfo = {}
-        
-        # Parse the file for user information
-        with (open(f"{fName}_{lName}.txt", "r") as userFile):
-            for line in userFile:
-                keyValue = line.split(":")
-                userInfo[keyValue[0]] = keyValue[1].strip()
-                
-        # Check if the password provided matches the password saved
-        if not pword == userInfo["password"]:
-            messagebox.showerror("Password Incorrect", "Your password didn't match. Please try again.")
-        else:
-            messageTitle = f"{fName} {lName} Account"
-            messageContent = f"Account found for {fName} {lName}"
-            messagebox.showinfo(messageTitle, messageContent)
-        
+
 
 mainWindow = Tk()
 mainWindow.title("User Registration App")
@@ -51,7 +42,9 @@ mainWindow.title("User Registration App")
 # Add a label to the application with instructions for the user
 # Complete the form and press submit to register a new user.
 
-instructions = Label(mainWindow, text="Complete the form and press submit to register a new user.")
+instructions = Label(
+    mainWindow, text="Complete the form and press submit to register a new user."
+)
 instructions.pack(padx=20, pady=10)
 
 # Create the frame for the first name components
@@ -85,7 +78,10 @@ DOBFrame.pack(padx=20, pady=10)
 
 pwFrame = Frame(mainWindow)
 pwLabel = Label(pwFrame, text="Password")
-pwEntry = Entry(pwFrame, show="*", )
+pwEntry = Entry(
+    pwFrame,
+    show="*",
+)
 
 pwLabel.pack(side=LEFT)
 pwEntry.pack(side=RIGHT)
@@ -95,11 +91,11 @@ pwFrame.pack(padx=20, pady=10)
 actionsFrame = Frame(mainWindow)
 
 # Change the saveBtn container from the main window to the actions frame
-saveBtn = Button(actionsFrame, text='Save User Information', command=saveUserInfo)
+saveBtn = Button(actionsFrame, text="Save User Information", command=saveUserInfo)
 saveBtn.pack(side=RIGHT, padx=20, pady=10)
 
 # Create a new Load button and add to the actions frame
-loadBtn = Button(actionsFrame, text='Load User Information', command=loadUserInfo)
+loadBtn = Button(actionsFrame, text="Load User Information", command=loadUserInfo)
 loadBtn.pack(side=LEFT, padx=20, pady=10)
 
 actionsFrame.pack()
